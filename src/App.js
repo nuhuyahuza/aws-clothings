ede2426493b79f95f940e93dda45ae3d39fe6c79
Shadow <nuhuyahuza@yahoo.com>
2022-11-15 18:01:27 -0800

    Added signin,form and custom button component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch , Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header-component';
-
+import SignINAndSignOut from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Switch>
           <Route exact  path='/' component = { HomePage   } />
           <Route  path='/shop' component = { ShopPage } />
+          <Route  path='/signin' component = { SignINAndSignOut } />
         </Switch>
     </div>
   );
